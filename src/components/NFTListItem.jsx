b2ef594ac1722863467e1237b555697c5bf632ef
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-10 09:46:59 +0100

    feat: hover state for honoraries

--- src/components/NFTListItem.jsx
+++ src/components/NFTListItem.jsx
@@ -8,7 +8,13 @@ const NFTListItem = ({ imgSrc, title }) => {
   if (!title) return <div className="hidden lg:block" />;
   return (
     <div className="w-full flex flex-col items-center gap-[10px] grid-item">
-      <Image src={imgSrc} alt="Honorary Claim 1" width={248} height={248} />
+      <Image
+        src={imgSrc}
+        alt="Honorary Claim 1"
+        width={248}
+        height={248}
+        className="honorary-image"
+      />
       <div className="p-[10px] text-[20px] bg-white text-black flex items-center gap-[10px] font-[700] w-fit">
         <StarIcon width={20} height={20} color="#FD6E2C" />
         {title}
