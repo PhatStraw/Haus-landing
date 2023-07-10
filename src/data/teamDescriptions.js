8b472a2536947464862d6cadb7daeea75aaaf9e1
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-10 14:09:13 +0100

    feat: move not on list and on list to honoraries page

--- src/data/teamDescriptions.js
+++ src/data/teamDescriptions.js
@@ -11,60 +11,70 @@ import bling from '../../public/images/cheddy.png';
 export const teamDescriptions = [
   {
     name: 'Just Megan',
+    id: 1,
     image: megan,
     description:
       "Megan's sharp as a tack and knows how to pickpocket like nobody's business. She used to make bank in fancy Manhattan bars, flirting with investment bankers and quants and swiping their Rolexes.",
   },
   {
     name: 'Cheddy',
+    id: 2,
     image: cheddy,
     description:
       "Cheddy's the richest dude here, and that's saying something. He started out like any other scumbag, buying and selling all sorts of illegal stuff with BTC back in 2013. But then he hit the jackpot.",
   },
   {
     name: 'Haxx',
+    id: 3,
     image: haxx,
     description:
       "Haxx is probably the most talented one in the crew. She got her nickname by hacking an early BTC mining operation and getting away clean. Since then, she's been combing smart contracts for lazy code.",
   },
   {
     name: 'Matrix',
+    id: 4,
     image: matrix,
     description:
       "Matrix is a paranoid dude who's always tripping out on something. He's got conspiracy theories about everything from aliens and pyramids to politics and the deep state. He's not someone you want to mess with.",
   },
   {
     name: 'Slick',
+    id: 5,
     image: slick,
     description:
       "Reformed leverage addict turned NFT influencer. Some people love him, but most people hate him. He's loaded and charges $5K per tweet to shill just about anything. He's also a sick snowboarder and one of the best in SoCal.",
   },
   {
     name: 'Bender',
+    id: 6,
     image: bender,
     description:
       "Bender's the cybersecurity whiz of the group. He used to work at Oracle right out of high school. He's got the highest IQ of the bunch. And even though he's got a wicked gambling addiction, he always plays by the book.",
   },
   {
     name: 'Stiix',
+    id: 7,
     image: stiix,
     description:
       "Stixx doesn't give a fuck about most things or people. He speaks his mind wether youre a fan of his opinion or not. Hes a sick gamer and makes six figures a year playing NBA 2K. He's also a degenerate gambler.",
   },
   {
     name: 'Chimney',
+    id: 8,
     image: chimney,
     description:
       'Loves the sound of a cash register. Trustfund baby with horrible vices. Spends most days of the week aping shitcoins and chain smoking cigarettes. Was hooked after 2017 after lucking out in crash and winning 7 BTC.',
   },
   {
     name: 'Ghost',
+    id: 9,
     image: ghost,
     description:
       "The Crypto God. He's the mastermind behind an anonymous crypto Twitter account with over 150K followers that you probably already follow. He's the one who's been rich forever.",
   },
   {
     name: 'Bling',
+    id: 10,
     image: bling,
     description:
       'Bling is an interesting character discovered and mentored by Cheddy in the early BTC days. Bling met Cheddy anonymously on an early Silk Road Forumn, and bought some dab carts from him originally.',
