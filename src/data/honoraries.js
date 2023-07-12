4b29da4faac34dad43d9c21fdec90f81977dac7d
Victor Whyte <victordavidwhyte@gmail.com>
2023-07-12 20:19:06 +0100

    fix: correct haus epxeriment link

--- src/data/honoraries.js
+++ src/data/honoraries.js
@@ -14,61 +14,61 @@ export const honoraries = [
     title: 'Adam Ape',
     image: AdamApe,
     tweet:
-      '🍊@Adam_Ape_ for Haus Experiment🍊\n\nArt always wins. Welcome to the Family, Adam. ⚙️\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@Adam_Ape_ for Haus Experiment🍊\n\nArt always wins. Welcome to the Family, Adam. ⚙️\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'Charlotte Fang',
     image: Charlotte,
     tweet:
-      '🍊@CharlotteFang77 Haus Experiment🍊\n\nOur Crowned Prince. Welcome to the Family, Charlie. 💕\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@CharlotteFang77 Haus Experiment🍊\n\nOur Crowned Prince. Welcome to the Family, Charlie. 💕\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'Ansem',
     image: Ansem,
     tweet:
-      '🍊@blknoiz06 for Haus Experiment🍊\n\nImagine letting niggas outwork you. Welcome to the family, Ansem. 🍸\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@blknoiz06 for Haus Experiment🍊\n\nImagine letting niggas outwork you. Welcome to the family, Ansem. 🍸\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'Gainzy',
     image: Gainzy,
     tweet:
-      "🍊@Gainzy222 for Haus Experiment🍊\n\nIt's easy to hate the rich, but do you have the courage to hate the poor?\n\nWelcome to the Family, Gainzy. 🎰\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫",
+      "🍊@Gainzy222 for Haus Experiment🍊\n\nIt's easy to hate the rich, but do you have the courage to hate the poor?\n\nWelcome to the Family, Gainzy. 🎰\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫",
   },
   {
     title: 'Degen Harambe',
     image: Harambe,
     tweet:
-      '🍊@DegenHarambe for Haus Experiment🍊\n\nChampion ape, who made it all back in one trade. Welcome to the Family, Z. 🎖️🦍\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@DegenHarambe for Haus Experiment🍊\n\nChampion ape, who made it all back in one trade. Welcome to the Family, Z. 🎖️🦍\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'ZK',
     image: ZK,
     tweet:
-      '🍊@ZK_shark for Haus Experiment🍊\n\nOrdinal.\n\nMaxi.\n\nBusiness.\n\nWelcome to the Family. 🔱\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@ZK_shark for Haus Experiment🍊\n\nOrdinal.\n\nMaxi.\n\nBusiness.\n\nWelcome to the Family. 🔱\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'DeeZe',
     image: Deeze,
     tweet:
-      '🍊@DeeZe for Haus Experiment 🍊\n\nArt collector, and dab connoisseur. Welcome to the Family. 🍯\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@DeeZe for Haus Experiment 🍊\n\nArt collector, and dab connoisseur. Welcome to the Family. 🍯\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'Cobie',
     image: Cobie,
     tweet:
-      '🍊@Cobie for Haus Experiment🍊\n\nThe original niche internet celebrity.  📸\n\nWe miss you, Cobie. 🧡\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@Cobie for Haus Experiment🍊\n\nThe original niche internet celebrity.  📸\n\nWe miss you, Cobie. 🧡\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {},
   {
     title: 'NotSoFast',
     image: NotSoFast,
     tweet:
-      '🍊@notsofast for Haus Experiment🍊\n\nBlack Lotus Destroyer. 🌹\n\nMilady Maxi. 💕\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@notsofast for Haus Experiment🍊\n\nBlack Lotus Destroyer. 🌹\n\nMilady Maxi. 💕\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
   {
     title: 'Keyboard Monkey',
     image: KeyboardMonkey,
     tweet:
-      '🍊@KeyboardMonkey3 for Haus Experiment🍊\n\nKing of roundtripping. 🚂\nHost of Rekt Radio.  📻\n\nKeep throwing down KBM. 🦍\n\nFollow the instructions on hausexperiment.com/claim to claim your honorary NFT 🔫',
+      '🍊@KeyboardMonkey3 for Haus Experiment🍊\n\nKing of roundtripping. 🚂\nHost of Rekt Radio.  📻\n\nKeep throwing down KBM. 🦍\n\nFollow the instructions on https://www.hausexperiment.com/honoraries to claim your honorary NFT 🔫',
   },
 ];
